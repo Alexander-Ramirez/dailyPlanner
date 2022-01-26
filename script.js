f93ed550d1eee5b73437fdf8f56509392e6212ae
Alex.Ramirez <aleramirez201@gmail.com>
2022-01-25 18:58:43 -0600

    got local storage to work and the save button function as well

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 let currentDay = moment();
 $("#currentDay").text(currentDay.format("dddd, MMMM Do, YYYY"));
+let currentTime = moment().hour();
+let saveBtn = $('button');
 
+saveBtn.on('click', function() {
+    let textContainer = $(this).parent().siblings();
+    let inputId = $(this).attr('id')
+    let textArea = textContainer.find(".textarea").val();
+    localStorage.setItem(inputId, textArea);
+    console.log(localStorage);
+});
+
+for (let i = 9; i < 18; i++) {
+    $("#" + i).text(localStorage.getItem(i));
+};
