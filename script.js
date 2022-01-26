6282d12db1f1f3daf48aeb5803d4c2435e8bf0e3
Alex.Ramirez <aleramirez201@gmail.com>
2022-01-25 19:52:45 -0600

    added color changing funtionality and changed a few text colors, also its done

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let currentDay = moment();
 $("#currentDay").text(currentDay.format("dddd, MMMM Do, YYYY"));
 let currentTime = moment().hour();
-let saveBtn = $('button');
 
+let saveBtn = $('button');
 saveBtn.on('click', function() {
     let textContainer = $(this).parent().siblings();
     let inputId = $(this).attr('id')
@@ -14,3 +14,15 @@ saveBtn.on('click', function() {
 for (let i = 9; i < 18; i++) {
     $("#" + i).text(localStorage.getItem(i));
 };
+
+let timeColors = $("textarea");
+timeColors.each(function () {
+    let time = $(this).attr('id'); 
+    if (currentTime > time) {
+        $(this).addClass('past');
+    } else if (currentTime === time) {
+        $(this).addClass('present');
+    } else {
+        $(this).addClass('future');
+    };
+});
